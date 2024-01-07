5e0e79578988bd1a54976d8491a156d47f2e6380
GCSLaoLi <cnlidong@live.cn>
2024-01-07 23:43:18 +0800

    增加github链接

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,10 @@
-import { defineUserConfig } from "vuepress";
+import { defaultTheme, defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
   lang: "zh-CN",
   title: "ChatgptShareServer",
   description: "基于官网UI的共享账号方案",
+  theme: defaultTheme({
+    repo: "xyhelper/chatgpt-share-server",
+  })
 });
