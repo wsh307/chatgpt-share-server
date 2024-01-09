b4acc42ca59d704fb4722da06dc46d5b0f7c866a
GCSLaoLi <cnlidong@live.cn>
2024-01-09 22:36:47 +0800

    调整适配GET及POST双模式

--- backend/openai/auth/auth.go
+++ backend/openai/auth/auth.go
@@ -10,5 +10,5 @@ func init() {
 	authGroup.POST("/login", Login)
 	authGroup.POST("/oauth", Oauth)
 	authGroup.GET("/logout", Logout)
-	authGroup.GET("/logintoken", LoginToken)
+	authGroup.ALL("/logintoken", LoginToken)
 }
