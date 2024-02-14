f1be581abed9a82786ac4acec8a657a710cab950
GCSLaoLi <cnlidong@live.cn>
2024-02-14 17:26:34 +0800

    更新标题生成

--- backend/openai/backend-api/gen_title.go
+++ backend/openai/backend-api/gen_title.go
@@ -4,11 +4,13 @@ import (
 	"backend/config"
 	"backend/modules/chatgpt/model"
 	"backend/utility"
+	"regexp"
 
 	"github.com/cool-team-official/cool-admin-go/cool"
 	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/text/gstr"
 )
 
 // GenTitle 生成会话标题
@@ -76,6 +78,25 @@ func GenTitle(r *ghttp.Request) {
 			"email":            carinfo.Email,
 			"chatgptaccountid": r.Header.Get("ChatGPT-Account-ID"),
 		})
+	}
+	message := respJson.Get("message").String()
+	// 如果message以 Conversation 开头，尝试正则解析 convid 和 title	Conversation a7de60d7-2002-49db-b429-a5e35a588380 already has title '爆米花为什么爆开'
+	if gstr.HasPrefix(message, "Conversation") {
+		// 正则解析
+		re := regexp.MustCompile(`Conversation (\S+) already has title '(.+)'`)
+		match := re.FindStringSubmatch(message)
+		if len(match) == 3 {
+			count, err := cool.DBM(model.NewChatgptConversations()).Where("convid", match[1]).Count()
+			if err ==nil && count == 0 {
+				cool.DBM(model.NewChatgptConversations()).Save(g.Map{
+					"convid":           match[1],
+					"title":            match[2],
+					"usertoken":        usertoken,
+					"email":            carinfo.Email,
+					"chatgptaccountid": r.Header.Get("ChatGPT-Account-ID"),
+				})
+			}
+
 	}
 	r.Response.WriteJsonExit(respBody)
 
